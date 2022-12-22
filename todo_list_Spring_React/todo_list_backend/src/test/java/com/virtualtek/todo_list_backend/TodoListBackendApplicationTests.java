fda6e30c54ae1092620ea3667bb25d902efe4b1a
Elcasvi <casvi.sanchez@gmail.com>
2022-12-22 17:14:25 -0600

    Creacion de proyecto spring

---
+++ todo_list_Spring_React/todo_list_backend/src/test/java/com/virtualtek/todo_list_backend/TodoListBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.virtualtek.todo_list_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoListBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
