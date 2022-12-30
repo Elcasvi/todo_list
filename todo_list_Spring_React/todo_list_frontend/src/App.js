b5da47d2fd701f82a260eba24696d2232eb6c917
Elcasvi <casvi.sanchez@gmail.com>
2022-12-29 21:26:25 -0600

    Hago la conexion al backend con axios para el login de usuarios

--- todo_list_Spring_React/todo_list_frontend/src/App.js
+++ todo_list_Spring_React/todo_list_frontend/src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [user,setUser]=useState(
+    {
+      username:"dev",
+      password:"Theshad0999"
+    }
+  )
+  var change=false;
+
+  useEffect(()=>
+  {
+    getUser()
+  },[])
+
+/*
+    useEffect(()=>{
+      axios.post("http://localhost:8080/api/login",user)
+      .then((response)=>
+      {
+        console.log("Todo bien!!!!!!!!")
+        console.log(response.data)
+      })
+      .catch((error)=>
+      {
+        console.log("Error!!!!!!!!!!!!!!!")
+        console.log(error.response.status);
+      })
+  })
+*/
+
+  
+  const getUser=async()=>
+  {
+    const myUser=await axios.post("http://localhost:8080/api/login",user)
+    .then((response)=>
+    {
+      console.log("Todo bien!!!!!!!!")
+      console.log(response.data)
+    })
+    .catch((error)=>
+    {
+      console.log("Error!!!!!!!!!!!!!!!")
+      console.log(error.response.status);
+    })
+    
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>Hello World!!!</p>
+      
     </div>
   );
 }
